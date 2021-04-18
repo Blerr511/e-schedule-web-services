deac1ee26e0a5d21603b475e599fb694c455f6b0
Blerr511 <lyovghukasyan@gmail.com>
2021-04-18 21:47:15 +0400

    aded index exprtoer to users controller

--- functions/src/controllers/users/index.ts
+++ functions/src/controllers/users/index.ts
@@ -1,11 +1,2 @@
-import * as admins from './admins';
-import * as students from './students';
-
-const usersController = {
-	admins,
-	students
-};
-
-export {admins, students};
-
-export default usersController;
+export * from './user.controller';
+export {default} from './user.controller';
---
+++ functions/src/controllers/users/user.controller.ts
@@ -0,0 +1,11 @@
+import * as admins from './admins';
+import * as students from './students';
+
+const usersController = {
+	admins,
+	students
+};
+
+export {admins, students};
+
+export default usersController;
